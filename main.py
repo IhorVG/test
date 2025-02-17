90c087e40d28eb0d2581dcc68cb05f71d2e2dfcb
IhorH <38507668+IhorVG@users.noreply.github.com>
2025-02-18 01:12:52 +0200

    Added 2 func

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import utils
 
+<<<<<<< Updated upstream
 print(utils.factorial(5))
+=======
+print(utils.factorial(5))
+print(utils.pow2(3))
+print(utils.pow3(3))
+>>>>>>> Stashed changes
--- utils.py
+++ utils.py
@@ -2,4 +2,14 @@ def factorial(n):
     if n == 0:
         return 1
     else:
+<<<<<<< Updated upstream
         return n*factorial(n-1)
+=======
+        return n*factorial(n-1)
+
+def pow2(n):
+    return n**2
+
+def pow3(n):
+    return n**3
+>>>>>>> Stashed changes
